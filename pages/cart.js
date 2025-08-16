611f1aad3f49b78d1a66ef5a7f0d9b03a5df4136
Amansahu83 <91147161+Amansahu83@users.noreply.github.com>
2025-08-17 05:06:09 +0530

    commite

--- pages/cart.js
+++ pages/cart.js
@@ -115,7 +115,7 @@ export default function Cart() {
                 <div className="flex-shrink-0">
                   {item.image_url ? (
                     <img
-                      src={`http://localhost:5000/api${item.image_url}`}
+                      src={`https://clothstore-backend.onrender.com${item.image_url}`}
                       alt={item.name}
                       className="w-24 h-24 object-cover rounded-lg"
                     />
